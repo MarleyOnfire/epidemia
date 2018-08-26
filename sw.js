51dbcb79a61904b83f2b3ec5e3c1324f1bcef13d
CircleCI <pspeter333@gmail.com>
2018-08-26 13:43:04 +0000

    2cb7eab03f8ae01d0dc411e3fe2a0fe6f0f451b7

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "7c0e3fe88782442c1cc0f18f60d4d278"
+    "revision": "b83020a05bf7aab72ec55e6490ed180a"
   },
   {
     "url": "life-buoy.4f62e248.png",

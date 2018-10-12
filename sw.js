f465bd1ce0581627aa153a241ac915cc0f2cacd0
CircleCI <pspeter333@gmail.com>
2018-10-12 23:30:14 +0000

    cb898c185b5827ba974a4f7ebcddb6ed7b17f3cb

--- sw.js
+++ sw.js
@@ -21,23 +21,23 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "9a1a7f54c3f9d00d10fa29918caad08a"
+    "revision": "b548e3e41995f5ba0cfebed200ff13c2"
   },
   {
-    "url": "logo_192.bda0fab5.png",
-    "revision": "08bb4fda6004ca86f87dffbaafeb137f"
+    "url": "logo_192.2f3849df.png",
+    "revision": "361656f0dbbe25687272cd81441cb0cc"
   },
   {
-    "url": "logo_512.9a49dbcc.png",
-    "revision": "a7c9fdeab82c30320c2f3be442ffc9d3"
+    "url": "logo_512.cb07ae21.png",
+    "revision": "2488cb81d20c1a67c47e9e2319c90d08"
   },
   {
     "url": "src.5aeadf68.js",
     "revision": "ae02bda0b8ba6dcbc8050cd180e95f26"
   },
   {
-    "url": "src.f9c8a4a5.css",
-    "revision": "d5d747d7437b131205f934b8935201b1"
+    "url": "src.f22c8e80.css",
+    "revision": "5294e05c57d99a8463b5f0589c901d63"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
